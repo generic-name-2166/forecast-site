9aed75a0608f509b5e959ca2eb606566da8022b2
generic-name-2166 <kirbesson277@mail.ru>
2024-07-18 22:50:57 +0300

    test: basic tests

--- src/forecast_site/root/tests.py
+++ src/forecast_site/root/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.utils.http import urlencode
 
-# Create your tests here.
+from .models import City
+
+
+def add_url_city(url: str, city: str) -> str:
+    return url + "?" + urlencode({"city": city})
+
+
+class CityModelTests(TestCase):
+    fixtures = ["city.json"]
+
+    def test_getting_city_by_name(self):
+        city_name = "Paris"
+        print(City.objects.all())
+        _city_obj = City.objects.get(name=city_name)
+
+
+class WeatherViewTests(TestCase):
+    fixtures = ["city.json"]
+
+    def test_main_page(self):
+        url = reverse("weather:index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_specifying_a_city(self):
+        city_name = "Paris"
+        url = add_url_city(reverse("weather:index"), city_name)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(city_name, response.content.decode())
+
+    def test_nonexistent_city(self):
+        fictional_city = "Los Santos"
+        url = add_url_city(reverse("weather:index"), fictional_city)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(fictional_city, response.content.decode())
--- src/forecast_site/root/views.py
+++ src/forecast_site/root/views.py
@@ -6,17 +6,16 @@ from .models import City
 
 
 def index(request: HttpRequest):
-    
     try:
         # idk what other type other than str can it be
-        city_name: str = request.GET["city"] # type: ignore
+        city_name: str = request.GET["city"]  # type: ignore
         city_obj: City = City.objects.get(name=city_name)
     except (KeyError, City.DoesNotExist):
         return render(request, "root/index.html")
-    
+
     city_obj.searches = F("searches") + 1
     city_obj.save()
 
-    context: dict[str, str] = { "city_name": city_name }
+    context: dict[str, str] = {"city_name": city_name}
 
     return render(request, "root/index.html", context=context)
