737011e2c7d41eaa0366f71489085e3329c4f531
generic-name-2166 <kirbesson277@mail.ru>
2024-07-18 16:51:09 +0300

    feat: model

--- src/forecast_site/root/models.py
+++ src/forecast_site/root/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    searches = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.name
